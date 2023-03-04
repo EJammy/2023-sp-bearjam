fb7a2f3d8742815a946ae39f601a6af90d9f585c
Connor Chang <connorc0627@gmail.com>
2023-03-03 22:16:05 -0800

    Test scene and initial player control

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody2D rb;
+    const float controlForce = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 force = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) {
+            force += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            force += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            force += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            force += Vector2.right;
+        }
+
+        rb.AddForce(force * controlForce);
+        Debug.Log(rb.velocity);
+    }
+}
