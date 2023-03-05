a146a97cb84ddf0a92c1d1cc9207e9b4a0f3f030
Connor Chang <connorc0627@gmail.com>
2023-03-05 14:38:07 -0800

    Time to panic

--- Assets/Scripts/PlayerWaterEffects.cs
+++ Assets/Scripts/PlayerWaterEffects.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerWaterEffects : MonoBehaviour
 {
+    Animator animator;
+
     [SerializeField] BoxCollider2D waterCheck;
     [SerializeField] GameObject light;
+
+    [SerializeField] RuntimeAnimatorController waterAnimator;
+    [SerializeField] RuntimeAnimatorController landAnimator;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -14,10 +19,13 @@ public class PlayerWaterEffects : MonoBehaviour
         {
             GetComponent<Rigidbody2D>().gravityScale = 0.3f;
             GetComponent<Rigidbody2D>().drag = 4;
+            GetComponent<Animator>().runtimeAnimatorController = waterAnimator;
         } else
         {
             GetComponent<Rigidbody2D>().gravityScale = 2;
             GetComponent<Rigidbody2D>().drag = 2;
+            GetComponent<Animator>().runtimeAnimatorController = landAnimator;
+            // GetComponent<Animation
         }
 
     }
